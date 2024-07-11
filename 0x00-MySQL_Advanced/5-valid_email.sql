3ed09959a72d4b2dacbdd3dbbaf96afc463d47b7
NORTH89 <nordintata0@gmail.com>
2024-07-11 20:30:38 +0000

    valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,14 @@
---vakudate email
-
-DELIMITER //
-CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+-- SQL script that creates a trigger that resets 
+-- the attribute valid_email only when the email has been changed.
+DELIMITER $$
+CREATE TRIGGER reset 
+BEFORE UPDATE
+ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END;//
-DELIMITER ;
+END $$
+
+DELIMITER;
