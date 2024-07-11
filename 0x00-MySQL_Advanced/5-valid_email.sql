338a842cb361b91ccb9cd15c48b58f4b6f59f7c6
NORTH89 <nordintata0@gmail.com>
2024-07-11 11:40:35 +0100

    valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
 --vakudate email
-DELIMITER // ;
-CREATE TRIGGER validate_email BEFORE UPDATE ON users
+
+DELIMITER //
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-	IF NEW.email <> OLD.email THEN
-		SET NEW.valid_email = 0;
-	END IF;
-END;
-//
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;//
 DELIMITER ;
